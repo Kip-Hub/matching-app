18f39542f6520109c0ab94133c706f03bdff8a0a
Kip-Hub <daniel.kip@hva.nl>
2021-02-02 18:36:58 +0100

    commit

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
 var camelcase = require('camelcase')
 
 console.log(camelcase('gewoon-een-test-toch-toch'))
-=======
-var camelcase = require('camelcase')
-
-console.log(camelcase('gewoon-een-test-toch'))
->>>>>>> 76c1030960e2cfde10ed6b7708595e8ac9086bbc
