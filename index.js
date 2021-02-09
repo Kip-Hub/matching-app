be5771429da655e432e94b3e22173fdcba7c477a
Kip-Hub <daniel.kip@hva.nl>
2021-02-09 17:15:04 +0100

    Server commit

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 const express = require('express');
 const port = 1900;
+const app = express();
 
+app.get('/', (req, res) => {
+    res.send('<h1>Hello world!</h1>');
+});
 
-express()
-    .get('/', onhome)
-    .get('/about', onabout)
-    .listen(port, () => {
-        console.log('Listening on port ' + port)
-    })
+app.get('/about', (req, res) => {
+    res.send('<h1>About me</h1>');
+});
 
+app.use(express.static('static/public'));
 
-function onhome(req, res) {
-    res.send('<h1>Hello world!</h1>\n')
-}
+app.use((req, res, ) => {
+    var img = '<img src= "/images/kat.jpg"/>';
+    res.send('<h1>404</h1>' + img);
+});
 
-function onabout(req, res) {
-    res.send('<h1>About me</h1>\n')
-}
+app.listen(port, () => {
+    console.log('Listening on port ' + port);
+});
