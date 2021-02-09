276d00af299c13b3a1d2af1a076ab8fe8ca974ec
Kip-Hub <daniel.kip@hva.nl>
2021-02-09 20:53:10 +0100

    static css commit

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const port = 1900;
 const app = express();
+const path = require('path');
 
 app.get('/', (req, res) => {
     res.send('<h1>Hello world!</h1>');
 });
 
-app.get('/about', (req, res) => {
-    res.send('<h1>About me</h1>');
+app.get('/profile', (req, res) => {
+    res.sendFile(path.join(__dirname, 'static/public', 'index.html'));
 });
 
 app.use(express.static('static/public'));
