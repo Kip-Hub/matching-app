fc18804b7f81d7f4d9b57262b1dcbbccdcefd25f
Kip-Hub <daniel.kip@hva.nl>
2021-02-09 16:07:30 +0100

    server commit

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-var camelcase = require('camelcase')
+const express = require('express');
+const port = 1900;
 
-console.log(camelcase('gewoon-een-test-toch'))
+
+express()
+    .get('/', onhome)
+    .get('/about', onabout)
+    .listen(port, () => {
+        console.log('Listening on port ' + port)
+    })
+
+
+function onhome(req, res) {
+    res.send('<h1>Hello world!</h1>\n')
+}
+
+function onabout(req, res) {
+    res.send('<h1>About me</h1>\n')
+}
