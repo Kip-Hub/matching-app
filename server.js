33ba6efa51acfaea9ebe88e7e8db06722fe941b5
Kip-Hub <daniel.kip@hva.nl>
2021-02-28 23:25:47 +0100

    read mongodb & ESLint install

--- server.js
+++ server.js
@@ -5,16 +5,12 @@ const port = 1900;
 const app = express();
 const path = require('path');
 const MongoClient = require('mongodb').MongoClient;
+var mongo = require('mongodb');
 const urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 require('dotenv').config()
 
 const url = 'mongodb+srv://' + process.env.DB_USER + ':' + process.env.DB_PASS + '@' + process.env.DB_HOST + '/' + process.env.DB_NAME + '?retryWrites=true&w=majority';
-
-MongoClient.connect(url, { useUnifiedTopology: true }, function(err, client) {
-    if (err) throw err
-    else console.log('Database connected?')
-})
 
 app.use(express.static('static/public'));
 
@@ -25,6 +21,22 @@ app.get('/', (req, res) => {
 });
 
 app.post('/', urlencodedParser, (req, res) => {
+    MongoClient.connect(url, { useUnifiedTopology: true }, function(err, client) {
+        if (err) {
+            throw err
+        } else {
+            const db = client.db(process.env.DB_NAME);
+            const collection = db.collection(process.env.DB_COLL);
+            collection.find({}).toArray(function(err, result) {
+                if (err) {
+                    throw err
+                } else {
+                    console.log(result);
+                }
+            });
+        }
+    })
+
     res.render('pages/result', { data: req.body });
 });
 
@@ -32,7 +44,7 @@ app.get('/profile/:userName', (req, res) => {
     res.render('pages/profile', { data: { userName: req.params.userName } });
 });
 
-app.use((req, res, ) => {
+app.get('*', (req, res, ) => {
     var img = '<img src= "/images/kat.jpg"/>';
     res.status(404).send('<h1>404 page not found</h1>' + img);
 });
