37315c11b787ed3a099f4b04e1e04e44d3227e35
Kip-Hub <daniel.kip@hva.nl>
2021-03-09 01:43:03 +0100

    Updated readme, added comments

--- README.md
+++ README.md
@@ -2,43 +2,52 @@
 
 ## description
 
+![homepage of the app](https://i.imgur.com/163fbnH.png)
+
 Matching-app is an app built as a project for Blok Tech at the Hogeschool van Amsterdam.
-The app is meant to help you find capable teammates for the games you play, rather than having to deal with randoms.
+The app is meant to help you find capable teammates for the games you play, rather than having to deal with randoms. The feature I've worked on for Blok Tech is filtering the data that the user searched for, and returning a user profile that matches the criteria. 
 
-## installing
+## Getting started
 
-In order to use this repository on your local device you should first clone it using the following link:  
+In order to use this repository on your local device you should first clone it by putting the following code into your terminal:  
 
-`https://github.com/Kip-Hub/matching-app.git`  
+```
+git clone https://github.com/Kip-Hub/matching-app.git
+```  
 
 
 After which you should install npm on your local device by running this code in your terminal:   
 
-`npm install`  
+```
+npm install
+```  
 
-Now for the dependancies that are used to run the project:
-
-`npm install ...`   
-`npm install ...`  
-`npm install ...`  
-
+This will automatically install the needed dependencies to run the project.
 
 ## Run scripts
 
-When succesfully installed the following scripts can be used:  
+When succesfully installed the following script can be used to start the project:  
 
-`npm start` 
+```
+npm start
+```
 
 
-## Built with
+## Dependencies used
 
-- Git
-- Ejs
-- ...
-- ...
+- ejs
+- express
+- mongodb
+- body-parser
+- dotenv
+
+## Wiki
+
+The research done during the project and the documentation of it can be found in the [Wiki](https://github.com/Kip-Hub/matching-app/wiki)
 
 ## License 
 
-This project is licensed under the MIT License - see the `LICENSE.md` file for details
+This project is licensed under the MIT License - see the [LICENSE.md](https://github.com/Kip-Hub/matching-app/blob/main/LICENSE) file for details
 
 
+ 
--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const url = 'mongodb+srv://' + process.env.DB_USER + ':' + process.env.DB_PASS +
 const client = new MongoClient(url, { useUnifiedTopology: true });
 const ObjectId = require("mongodb").ObjectID;
 
+
+// Database connection with MongoDB
 async function mongoConnect() {
     try {
         await client.connect()
@@ -19,14 +21,18 @@ async function mongoConnect() {
 }
 mongoConnect();
 
+// Middleware to access static files such as CSS
 app.use(express.static('static/public'));
 
+// Setting the view engine for templating to be handled by EJS
 app.set('view engine', 'ejs');
 
+// Home page
 app.get('/', (req, res) => {
     res.render('pages/index');
 });
 
+// Post request handled when form on homepage is submitted
 app.post('/', urlencodedParser, (req, res) => {
     const db = client.db(process.env.DB_NAME);
     const users = db.collection(process.env.DB_COLL);
@@ -36,6 +42,7 @@ app.post('/', urlencodedParser, (req, res) => {
         playertype: req.body.playertype
     };
 
+    // Find result matching the submitted criteria
     users.find(query).toArray((err, result) => {
         if (err) {
             throw err
@@ -45,11 +52,13 @@ app.post('/', urlencodedParser, (req, res) => {
     })
 });
 
+// Get request for the profile page
 app.get('/profile', (req, res) => {
     const db = client.db(process.env.DB_NAME);
     const users = db.collection(process.env.DB_COLL);
     const id = { _id: ObjectId("60457d71f75d0ee05a6618fd") };
 
+    // Find and show the user who is "logged in"
     users.find(id).toArray((err, result) => {
         if (err) {
             throw err
@@ -59,10 +68,12 @@ app.get('/profile', (req, res) => {
     })
 });
 
+// get request for the edit page (edit data on the profile)
 app.get('/edit', (req, res) => {
     res.render('pages/edit');
 });
 
+// post request handles the submitted form with the new data
 app.post('/edit', urlencodedParser, (req, res) => {
     const db = client.db(process.env.DB_NAME);
     const users = db.collection(process.env.DB_COLL);
@@ -82,8 +93,10 @@ app.post('/edit', urlencodedParser, (req, res) => {
     const filter = { _id: ObjectId("60457d71f75d0ee05a6618fd") };
     const options = { upsert: true };
 
+    // updates the existing data to the newly submitted data
     users.updateOne(filter, document, options);
 
+    // renders the updated profile afterwards
     users.find(filter).toArray((err, result) => {
         if (err) {
             throw err
@@ -94,11 +107,13 @@ app.post('/edit', urlencodedParser, (req, res) => {
 
 });
 
+// handles any non existing paths, and shows a 404 page
 app.get('*', (req, res, ) => {
     var img = '<img src= "/images/kat.jpg"/>';
     res.status(404).send('<h1>404 page not found</h1>' + img);
 });
 
+// App is "listening" to http requests on port 1900
 app.listen(port, () => {
     console.log('Listening on port ' + port);
 });
